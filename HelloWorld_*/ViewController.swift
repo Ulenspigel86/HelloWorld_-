8a4ecfb4ded556efa8972adb96b31a2422a693fa
Ulenspigel86 <66313198+Ulenspigel86@users.noreply.github.com>
2024-09-30 15:01:10 +0300

    Initial Commit

---
+++ HelloWorld_*/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HelloWorld_*
+//
+//  Created by Артем Иванов on 30.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
